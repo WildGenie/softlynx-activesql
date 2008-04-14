6b1dd48cc809016b8e7c5fed6b07b3b766d9a108
kot <kot@55ac7930-24d2-11de-8c12-6dbec8df7899>
2008-04-14 18:51:43 +0000

    Изменен метод записи в XML объекта SimpleConfig

--- trunk/SQLiteDataset/SQLiteDataset/SimpleConfig/ConfigParams.cs
+++ trunk/SQLiteDataset/SQLiteDataset/SimpleConfig/ConfigParams.cs
@@ -3,64 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Xml;
-using System.Xml.Schema;
-using System.Xml.Serialization;
 using System.IO;
 
+namespace Softlynx.SimpleConfig
 
-namespace Softlynx.SQLiteDataset.SimpleConfig
-{
-    [Serializable]
-    [XmlRoot("SimpleConfig")]
-    public class SerializableHashtable: Dictionary<String,String>, IXmlSerializable
-    {
-        
-        XmlSchema IXmlSerializable.GetSchema()
-        {
-            return null;
-        }
- 
-        void IXmlSerializable.ReadXml(XmlReader reader)
-        { 
-            reader.Read();
-            while (reader.Name == "parameter")
-            {
-                string key=reader.GetAttribute("name");
-                string value =reader.GetAttribute("value");
-                this[key] = value;
-                reader.Read();
-            }
-        }
-
-        void IXmlSerializable.WriteXml(XmlWriter writer)
-        {
-            foreach (string key in Keys)
-            {
-
-                string value = this[key];
-                writer.WriteStartElement("parameter");
-                writer.WriteAttributeString("name", key.ToString());
-                writer.WriteAttributeString("value", value.ToString());
-                writer.WriteEndElement();
-            }
-        }
-    }
-
+{        
     public static class SimpleConfig
     {
-        internal static XmlSerializer formatter = null;
-        public static SerializableHashtable Pairs = new SerializableHashtable();
         public static string FileName=string.Empty;
+        public static Dictionary<String,String> Pairs=new Dictionary<string,string>();
 
-        static SimpleConfig()
-        {
-            formatter = new XmlSerializer(typeof(SerializableHashtable));
-        }
+
         public static void Save()
         {
-            using (FileStream strm = new FileStream(FileName,FileMode.Create))
+            using (FileStream strm = new FileStream(FileName, FileMode.Create))
             {
-                formatter.Serialize(strm, Pairs);
+                using (XmlWriter writer = new XmlTextWriter(strm, null))
+                {
+                    writer.WriteStartElement("SimpleConfig");
+                    foreach (string key in Pairs.Keys)
+                    {
+                        string value = Pairs[key];
+                        writer.WriteStartElement("parameter");
+                        writer.WriteAttributeString("name", key);
+                        writer.WriteAttributeString("value", value);
+                        writer.WriteEndElement();
+                    }
+                    writer.WriteEndElement();
+                    writer.Close();
+
+                }
                 strm.Close();
             }
         }
@@ -69,7 +41,21 @@ namespace Softlynx.SQLiteDataset.SimpleConfig
         {
             using (FileStream strm = new FileStream(FileName, FileMode.Open))
             {
-                Pairs=(SerializableHashtable)formatter.Deserialize(strm);
+                Pairs.Clear();
+                using (XmlReader reader = new XmlTextReader(strm))
+                {
+                    reader.ReadStartElement("SimpleConfig");
+                    while (reader.Name == "parameter")
+                    {
+                        string key = reader.GetAttribute("name");
+                        string value = reader.GetAttribute("value");
+                        Pairs[key] = value;
+                        reader.Read();
+                    }
+                    reader.ReadEndElement();
+                    reader.Close();
+                }
+                    
                 strm.Close();
             }
         }
