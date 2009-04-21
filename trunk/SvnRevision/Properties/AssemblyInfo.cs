fb5e8693e35401a48b6b51a241090478b1b26eeb
Alexei.Volkov@softlynx.ru <Alexei.Volkov@softlynx.ru@55ac7930-24d2-11de-8c12-6dbec8df7899>
2009-04-21 21:21:34 +0000

    Tools to automate AssemblyInfo.cs handling with sync to Subversion repository.

---
+++ trunk/SvnRevision/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SvnRevision")]
+[assembly: AssemblyDescription("Handle  AssemblyInfo according to subversion revision.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SoftLynx")]
+[assembly: AssemblyProduct("SvnRevision")]
+[assembly: AssemblyCopyright("Copyright © SoftLynx 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f15bb532-f2f1-4497-8d7c-000851777d65")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.85.0")]
+[assembly: AssemblyFileVersion("1.0.85.0")]
