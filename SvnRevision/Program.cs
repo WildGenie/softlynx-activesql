d7a4f48f02e4c155f0a06e8d08a08c7aa2de6611
Alexei.Volkov <Alexei.Volkov@softlynx.ru>
2009-04-21 21:21:34 +0000

    Tools to automate AssemblyInfo.cs handling with sync to Subversion repository.

---
+++ SvnRevision/Program.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.IO;
+
+namespace SvnRevision
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: SvnRevision.exe <folder> [path to AssemblyInfo.cs]");
+                return -1;
+            }
+            try
+            {
+                ProcessStartInfo pi = new ProcessStartInfo();
+                pi.RedirectStandardOutput = true;
+                pi.WorkingDirectory = args[0];
+                pi.FileName = "svnversion";
+                pi.CreateNoWindow = true;
+                pi.UseShellExecute = false;
+                Process proc = System.Diagnostics.Process.Start(pi);
+                int version = 0;
+                int delta = 0;
+                while (!proc.StandardOutput.EndOfStream)
+                {
+                    string line = proc.StandardOutput.ReadLine();
+                    if (line.EndsWith("M", StringComparison.OrdinalIgnoreCase))
+                        delta += 1;
+                    int.TryParse(line.Trim('M'), out version);
+                }
+                proc.WaitForExit();
+                version += delta;
+                Console.WriteLine(version.ToString());
+                string subfile=Path.Combine(pi.WorkingDirectory, args.Length>1?args[1]:@"Properties\assemblyinfo.cs");
+                string[] lines=File.ReadAllLines(subfile,Encoding.UTF8);
+                List<string> outlines = new List<string>();
+                bool modified = false;
+                Regex pattern = new Regex(@"(AssemblyVersion|AssemblyFileVersion)\(""(.*)""\)",
+                    RegexOptions.Compiled);
+                foreach (string code in lines)
+                {
+                    string s=code;
+                    Match m = pattern.Match(s);
+                    if (m.Success)
+                    {
+                        List<string> components = new List<string>(m.Groups[2].Value.Split('.'));
+                        while (components.Count < 4) components.Add(string.Empty);
+                        components[2] = version.ToString();
+                        s = m.Result("$`$1(\"" + string.Join(".", components.ToArray()) + "\")$'");
+                        if (s!=code)
+                            modified = true;
+                    }
+                    outlines.Add(s);
+                }
+                if (modified)
+                    File.WriteAllLines(subfile, outlines.ToArray(), Encoding.UTF8);
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -2;
+            }
+        }
+    }
+}
